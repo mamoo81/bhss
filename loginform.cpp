f9c5a2b719c9cd03d75d4813dd2258fc355ec979
mamoo <user@milislinux.org>
2021-11-23 20:38:30 +0300

    postgresql servis kontrolü eklendi.

--- loginform.cpp
+++ loginform.cpp
@@ -5,6 +5,7 @@
 #include "veritabani.h"
 //*******************************
 #include <QCloseEvent>
+#include <QSqlDatabase>
 
 
 LoginForm::LoginForm(QWidget *parent)
--- main.cpp
+++ main.cpp
@@ -20,84 +20,51 @@ int main(int argc, char *argv[])
     db.setHostName("localhost");
     db.setUserName("postgres");
     db.setPassword("postgres");
-    if(!db.open())
-        qFatal("veritabanı bağlantı hatası: %s",
-            qPrintable(db.lastError().text()));
-
-    //localizasyon default türkçe ayarlama.
-    QLocale l(QLocale::Turkish, QLocale::Turkey);
-    QLocale::setDefault(l);
-    a.setOrganizationName("milis");
-    a.setApplicationName("mhss");
-    // .config altında mhss klasörü varmı kontrol
-    auto dizin = QStandardPaths::writableLocation(QStandardPaths::ConfigLocation) + "/" + QCoreApplication::applicationName();
-    if(!QFileInfo::exists(dizin)){
-        QDir().mkdir(dizin);// mhss klasörünü oluşturma.
-        // mhss klasörü içinde hizlibutonlar.ini dosyasını oluşturma başlangıcı.
-        QFile hizlibutonlarini(dizin + "/hizlibutonlar.ini");
-        if(!QFileInfo::exists(hizlibutonlarini.fileName())){
-            hizlibutonlarini.open(QIODevice::ReadWrite);
+    if(db.open()){
+        //localizasyon default türkçe ayarlama.
+        QLocale l(QLocale::Turkish, QLocale::Turkey);
+        QLocale::setDefault(l);
+        a.setOrganizationName("milis");
+        a.setApplicationName("mhss");
+        // .config altında mhss klasörü varmı kontrol
+        auto dizin = QStandardPaths::writableLocation(QStandardPaths::ConfigLocation) + "/" + QCoreApplication::applicationName();
+        if(!QFileInfo::exists(dizin)){
+            QDir().mkdir(dizin);// mhss klasörünü oluşturma.
+            // mhss klasörü içinde hizlibutonlar.ini dosyasını oluşturma başlangıcı.
+            QFile hizlibutonlarini(dizin + "/hizlibutonlar.ini");
+            if(!QFileInfo::exists(hizlibutonlarini.fileName())){
+                hizlibutonlarini.open(QIODevice::ReadWrite);
+            }
         }
+        else{
+            // mhss klasörü içinde hizlibutonlar.ini dosyasını oluşturma başlangıcı.
+            QFile hizlibutonlarini(dizin + "/hizlibutonlar.ini");
+            if(!QFileInfo::exists(hizlibutonlarini.fileName())){
+                hizlibutonlarini.open(QIODevice::ReadWrite);
+            }
+        }
+        QTranslator translator;
+        const QStringList uiLanguages = QLocale::system().uiLanguages();
+        for (const QString &locale : uiLanguages) {
+            const QString baseName = "mhss_" + QLocale(locale).name();
+            if (translator.load(":/i18n/" + baseName)) {
+                a.installTranslator(&translator);
+                break;
+            }
+        }
+        LoginForm w;
+        w.show();
+        return a.exec();
     }
     else{
-        // mhss klasörü içinde hizlibutonlar.ini dosyasını oluşturma başlangıcı.
-        QFile hizlibutonlarini(dizin + "/hizlibutonlar.ini");
-        if(!QFileInfo::exists(hizlibutonlarini.fileName())){
-            hizlibutonlarini.open(QIODevice::ReadWrite);
-        }
+        QMessageBox msg(0);
+        msg.setWindowTitle("Uyarı");
+        msg.setText("PostgreSQL servisini kontrol edin.");
+        msg.setIcon(QMessageBox::Information);
+        msg.setDefaultButton(QMessageBox::Ok);
+        msg.setStandardButtons(QMessageBox::Ok);
+        msg.setButtonText(QMessageBox::Ok, "Tamam");
+        msg.exec();
+        exit(0);
     }
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "mhss_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
-    }
-    LoginForm w;
-    w.show();
-    return a.exec();
-    // program birden fazla çalışmaması için kontrol
-    // QSharedMemory shared(QApplication::applicationName());
-//    QSharedMemory shared("dfjvs3232g-as5f3sd23a-asd22ef");
-//    if(!shared.create(512, QSharedMemory::ReadWrite)){
-//        QMessageBox msg(0);
-//        msg.setWindowTitle("Uyarı");
-//        msg.setText("Program zaten çalışıyor.");
-//        msg.setIcon(QMessageBox::Information);
-//        msg.setDefaultButton(QMessageBox::Ok);
-//        msg.setStandardButtons(QMessageBox::Ok);
-//        msg.setButtonText(QMessageBox::Ok, "Tamam");
-//        msg.exec();
-//        exit(0);
-//    }
-//    else{
-
-//        QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL", "mhss_data");
-//        db.setHostName("localhost");
-//        db.setUserName("postgres");
-//        db.setPassword("postgres");
-//        if(!db.open())
-//            qFatal("veritabanı bağlantı hatası: %s",
-//                qPrintable(db.lastError().text()));
-//        Veritabani vt;
-//        if(!vt.veritabaniVarmi())
-//        {
-//            QMessageBox msg(0);
-//            msg.setText("Veritabanı bulunamadı.\n\nSıfırdan oluşturmak ister misiniz?");
-//            msg.setWindowTitle("Dikkat");
-//            msg.setIcon(QMessageBox::Question);
-//            msg.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
-//            msg.setDefaultButton(QMessageBox::No);
-//            msg.setButtonText(QMessageBox::Yes, "Evet");
-//            msg.setButtonText(QMessageBox::No, "Hayır");
-//            int cevap = msg.exec();
-//            switch (cevap) {
-//            case QMessageBox::Yes:
-//                vt.veritabaniOlustur();
-//                break;
-//            }
-//        }
-//    }
 }
