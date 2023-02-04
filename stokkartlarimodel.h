619cc6d317ca4954ed52332350a3808da4f5a5ee
mamoo <mehmet@basat.dev>
2023-02-04 17:52:25 +0300

    fix

--- stokkartlarimodel.h
+++ stokkartlarimodel.h
@@ -21,7 +21,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 */
 #ifndef STOKKARTLARIMODEL_H
 #define STOKKARTLARIMODEL_H
-#include "veritabani.h"
 #include "stokyonetimi.h"
 //*******************************
 #include <QObject>
